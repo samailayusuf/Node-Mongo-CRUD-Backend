94116a2497149f0ac8aabbe1739c8b958731e511
samailayusuf <yusuf7samaila@gmail.com>
2020-06-17 17:02:12 +0100

    added: user authentication

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require ('mongoose');
 
 const stuffRouter = require('./routes/stuff');
+const userRouter = require('./routes/user');
 
 const app = express();
 
@@ -26,9 +27,10 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
 
   app.use('/api/stuff', stuffRouter);
+  app.use('/api/auth', userRouter);
 
 
 
