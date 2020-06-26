3e6fe4e91ee80a1eab212bfba4a7c1f3eff65a7f
samailayusuf <yusuf7samaila@gmail.com>
2020-06-27 00:19:04 +0100

    format: user.js

--- controllers/user.js
+++ controllers/user.js
@@ -3,20 +3,18 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.password, 10).then(
-      (hash) => {
+    bcrypt.hash(req.body.password, 10).then( (hash) => {
         const user = new User({
           email: req.body.email,
           password: hash
         });
-        user.save().then(
-          () => {
+
+        user.save().then(() => {
             res.status(201).json({
               message: 'User added successfully!'
             });
           }
-        ).catch(
-          (error) => {
+        ).catch((error) => {
             res.status(500).json({
               error: error
             });
