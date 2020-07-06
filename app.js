79a5d8e89bdcd0ba951e39268195233b4e243219
samailayusuf <yusuf7samaila@gmail.com>
2020-07-06 17:57:06 +0100

    Fix: handling image route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require ('mongoose');
+const path = require('path');
 
 const stuffRouter = require('./routes/stuff');
 const userRouter = require('./routes/user');
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
     next();
 });
 
+  app.use('/images', express.static(path.join(__dirname, 'images')));
   app.use('/api/stuff', stuffRouter);
   app.use('/api/auth', userRouter);
 
