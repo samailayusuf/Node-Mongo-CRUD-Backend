74ba9545066bfe52aa4ab4b6f3af744f2e932bc2
Yusuf Samaila <47528531+samailayusuf@users.noreply.github.com>
2021-07-30 09:43:35 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+//defining user schema
 const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
 });
 
+//plugging unique validator to user schema
 userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
