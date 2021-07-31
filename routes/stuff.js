90d07db6ca4812efeb367391cef660362c8182ac
Yusuf Samaila <47528531+samailayusuf@users.noreply.github.com>
2021-07-31 21:33:20 +0100

    Update stuff.js

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
+const auth = require('../middleware/auth'); //importing Authorization middleware 
+const multer = require('../middleware/multer-config'); //importing the multer upload iddleware
 
-const stuffCtrl = require('../controllers/stuff');
+const stuffCtrl = require('../controllers/stuff'); //importing stuff controller
 
-router.get('/', auth, stuffCtrl.getAllStuff);
-router.post('/', auth, multer, stuffCtrl.createThing);
-router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
-router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.get('/', auth, stuffCtrl.getAllStuff); //route to handle the '/' path including authorization
+router.post('/', auth, multer, stuffCtrl.createThing); //route to handle a post request, creating a thing with upload middleware set
+router.get('/:id', auth, stuffCtrl.getOneThing); //route to get only one thing with id
+router.put('/:id', auth, stuffCtrl.modifyThing); //route to update one thing using the id
+router.delete('/:id', auth, stuffCtrl.deleteThing); // route to delete thing using the id
 
 module.exports = router;
