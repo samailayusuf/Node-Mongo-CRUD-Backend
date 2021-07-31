f928a8ac71ac9742e5cc2e68bb0021e46ed745fa
Yusuf Samaila <47528531+samailayusuf@users.noreply.github.com>
2021-07-31 21:28:42 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const userCtrl = require('../controllers/user');
+const userCtrl = require('../controllers/user'); //importing the user controller
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', userCtrl.signup); //assigning the signup controller to signup route
+router.post('/login', userCtrl.login); //assigning the login controller to login route
 
 module.exports = router;
